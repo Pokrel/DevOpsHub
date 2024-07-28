619b784fc62cb741bfc4d2ce90934d92b5082a82
Gyaneshp <gyan.charm@gmail.com>
2024-07-28 17:49:32 +1000

    add active for index page

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const content = document.getElementById("content");
     const topnavLinks = document.querySelectorAll(".topnav a");
-    const sidebarLinks = document.querySelectorAll(".sidebar ul li a");
-    let currentSection = "python";
+    let currentSection = "home";
     
     const pages = {
+        home: ["home_intro", "about", "policy", "contribute"],
         python: ["python_intro", "python_syntax", "python_comments", "python_variables"],
         css: ["css_intro", "css_syntax", "css_selectors", "css_colors"],
         javascript: ["js_intro", "js_syntax", "js_variables", "js_functions"],
@@ -45,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function highlightCurrentTopic(currentPage) {
+        const sidebarLinks = document.querySelectorAll(".sidebar ul li a");
         sidebarLinks.forEach(link => {
             const page = link.getAttribute("href").split("/").pop().replace(".html", "");
             if (page === currentPage) {
@@ -55,24 +56,28 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    function highlightCurrentMenu() {
+        topnavLinks.forEach(link => {
+            const section = link.getAttribute("href").split("/").pop().replace(".html", "");
+            if (section === currentSection) {
+                link.classList.add("active");
+            } else {
+                link.classList.remove("active");
+            }
+        });
+    }
+
     topnavLinks.forEach(link => {
         link.addEventListener("click", function(event) {
             event.preventDefault();
-            const section = this.getAttribute("href").split("/").pop().replace(".html", "");
-            currentSection = section;
-            const firstPage = pages[section] ? pages[section][0] : "python_intro";
+            currentSection = this.getAttribute("href").split("/").pop().replace(".html", "");
+            highlightCurrentMenu();
+            const firstPage = pages[currentSection] ? pages[currentSection][0] : "home_intro";
             loadContent(firstPage);
         });
     });
 
-    sidebarLinks.forEach(link => {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-            const page = this.getAttribute("href").split("/").pop().replace(".html", "");
-            loadContent(page);
-        });
-    });
-
-    const initialPage = pages[currentSection] ? pages[currentSection][0] : "python_intro";
+    const initialPage = pages[currentSection] ? pages[currentSection][0] : "home_intro";
     loadContent(initialPage);
+    highlightCurrentMenu();
 });
